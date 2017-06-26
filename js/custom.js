cdf9bfffadd1370c23722d15ecdd33679ef5a124
mrmedina87 <mr_medina87@yahoo.com.ar>
2017-06-26 13:30:23 +0200

    new Form for adding products and upddating

--- js/custom.js
+++ js/custom.js
@@ -3,19 +3,99 @@ const apiRestUrl = "http://products:8080/api";
 
 const buttonDelete = $('<button>Delete</button>');
 buttonDelete.addClass(["btn","btn-primary"]).attr("type","button").attr("data-toggle","modal").attr("data-target","#deleteConfirmation");
-
 const buttonDeleteModal = $('<button type="button" class="btn btn-primary">Yes</button>');
+
+const buttonEdit = $('<button>Edit</button>');
+buttonEdit.addClass(["btn","btn-primary"]).attr("type","button").attr("data-toggle","modal").attr("data-target","#formProductModal");
+const buttonSaveModal = $('<button type="button" class="btn btn-primary">Save</button>');
+
+$("#newprodbutton").click(editProduct.bind({isnew: true}));
+
+$('.WYSIWYG-editor').froalaEditor({
+  toolbarButtons: ['fullscreen', 'bold', 'italic', 'underline', 'strikeThrough', 'subscript', 'superscript', '|', 'fontFamily', 'fontSize', 'color'],
+  pluginsEnabled: null
+});
+
+/*
+, 'inlineStyle', 'paragraphStyle', '|', 'paragraphFormat', 'align', 'formatOL', 'formatUL', 'outdent', 'indent', 'quote', '-', 'insertLink', 'insertImage', 'insertVideo', 'insertFile', 'insertTable', '|', 'emoticons', 'specialCharacters', 'insertHR', 'selectAll', 'clearFormatting', '|', 'print', 'help', 'html', '|', 'undo', 'redo'
+*/
 
 function deleteProduct() {
   const prodId = this.products_id;
-  $.ajax({
-    method: "DELETE",
-    url: apiRestUrl + "/product/" + prodId
-  })
-  .done(function(data) {
-    $('#deleteConfirmation').modal("hide");
-    updateList();
+  $("#yesDeleteButton").remove();
+  let yesDeleteModal = buttonDeleteModal.clone().attr("id", "yesDeleteButton");
+
+  yesDeleteModal.click(function() {
+    console.log(prodId);
+    $.ajax({
+      method: "DELETE",
+      url: apiRestUrl + "/product/" + prodId
+    })
+    .done(function(data) {
+      $('#deleteConfirmation').modal("hide");
+      updateList();
+    });
   });
+
+  $("#deleteConfirmation .modal-footer").append(yesDeleteModal)
+}
+
+function editProduct() {
+  let binded = this;
+  let isUpdate = true;
+  let prodId = "";
+  if(binded.isnew) {
+    isUpdate = false;
+    $("#editModalLabel").text("Create Product");
+  } 
+  else {
+    prodId = binded.products_id;
+    $("#editModalLabel").text("Edit Product");
+  }
+  $("#yesSaveProduct").remove();
+  let yesSaveProduct = buttonSaveModal.clone().attr("id", "yesSaveProduct");
+
+  yesSaveProduct.click(function() {
+    let wysiwygEditorsFilled = true;
+    let wysiwygEditors = $('.WYSIWYG-editor');
+    let inputsFilled = true;
+    let productInputs = $("input.form-control");
+    let wysiwygEditor1 = $($('.WYSIWYG-editor')[0]);
+    
+    for (let i = wysiwygEditors.length - 1; i >= 0; i--) {
+      if($(wysiwygEditors[i]).froalaEditor('core.isEmpty')) {
+        wysiwygEditorsFilled = false;  
+      }
+    }
+    for (let i = productInputs.length - 1; i >= 0; i--) {
+      if($(productInputs[i]).val() === "") {
+        inputsFilled = false;
+      }
+    }
+    if(wysiwygEditorsFilled && inputsFilled) {
+      if(isUpdate) {
+        console.log("put");
+      }
+      else {
+        console.log("post"); 
+      }
+      $('#formProductModal').modal("hide");
+    }
+    else {
+      alert("All the fields are required");
+    }
+
+    /*$.ajax({
+      method: "DELETE",
+      url: apiRestUrl + "/product/" + prodId
+    })
+    .done(function(data) {
+      $('#deleteConfirmation').modal("hide");
+      updateList();
+    });*/
+  });
+
+  $("#formProductModal .modal-footer").append(yesSaveProduct)
 }
 
 function updateList() {
@@ -31,12 +111,13 @@ function updateList() {
         .append($("<span class='prod-price'>" + product.products_price + "</span>"));
 
       let deleteButton = buttonDelete.clone();
-
-      $("#yesDeleteButton").remove();
-      let yesDeleteModal = buttonDeleteModal.clone().attr("id", "yesDeleteButton");
-      yesDeleteModal.click(deleteProduct.bind(product));
-      $("#deleteConfirmation .modal-footer").append(yesDeleteModal);
+      deleteButton.click(deleteProduct.bind(product));
       productElement.append(deleteButton);
+
+      let editButton = buttonEdit.clone();
+      editButton.click(editProduct.bind(product));
+      productElement.append(editButton);
+
       productsList.append(productElement);
     });
   });
--- main.php
+++ main.php
@@ -1,5 +1,19 @@
 <?php 
 
+function getLanguages() {
+  $link = mysqli_connect('localhost', 'root', '', 'dury');
+  mysqli_set_charset($link,'utf8');
+
+  $result = mysqli_query($link, "select * from languages");
+  $langs = array();
+  for ($i = 0; $i < mysqli_num_rows($result); $i++) {
+    $langs[$i] = mysqli_fetch_object($result);
+  }
+
+  mysqli_close($link);
+  return $langs;
+}
+
 function showMain() {
   ?> 
   <html>
@@ -8,6 +22,15 @@ function showMain() {
       <title>Products</title>
       <link rel="stylesheet" href="assets/bootstrap4.min.css">
       <link rel="stylesheet" href="assets/styles.css">
+
+      <!-- Froala external style dependencies -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.4.0/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
+      <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.css">
+   
+      <!-- Froala Styles -->
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.6.0/css/froala_editor.pkgd.min.css" rel="stylesheet" type="text/css" />
+      <link href="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.6.0/css/froala_style.min.css" rel="stylesheet" type="text/css" />
+
     </head>
     <body>
       <section class="container">
@@ -18,6 +41,7 @@ function showMain() {
             <span class"header-price">Price</span>
             <ul class="products-list">
             </ul>
+            <button type="button" class="btn btn-primary" data-toggle="modal" data-target="#formProductModal" id="newprodbutton">New Product</button>
           </div>
         </div>
       </section>
@@ -27,7 +51,7 @@ function showMain() {
         <div class="modal-dialog" role="document">
           <div class="modal-content">
             <div class="modal-header">
-              <h5 class="modal-title" id="exampleModalLabel">Delete Confirmation</h5>
+              <h5 class="modal-title" id="deleteModalLabel">Delete Confirmation</h5>
               <button type="button" class="close" data-dismiss="modal" aria-label="Close">
                 <span aria-hidden="true">&times;</span>
               </button>
@@ -43,9 +67,67 @@ function showMain() {
         </div>
       </div>
 
+      <!-- Modal: Form Edit product-->
+      <div class="modal fade" id="formProductModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-lg" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title" id="editModalLabel">Product</h5>
+              <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+              </button>
+            </div>
+            <div class="modal-body">
+              <form id="product-data" role="form" data-toggle="validator">
+                <div class="form-group">
+                  <label for="product-reference">Reference</label>
+                  <input type="text" id="product-reference" placeholder="Reference Code" class="form-control" required>
+                </div>
+                <div class="form-group">
+                  <label for="product-price">Price</label>
+                  <input type="number" id="product-price" placeholder="Price" class="form-control" required>
+                </div>
+                <?php
+                $langs = getLanguages();
+                foreach ($langs as $lang) {
+                ?>
+                <fieldset class="form-group lang-box-wrapper" lang-id="<?php echo $lang->languages_id; ?>">
+                  <legend><?php echo $lang->languages_name; ?></legend>
+                  <label for="name-product-<?php echo strtolower($lang->languages_name); ?>">Name</label>
+                  <input class="form-control" type="text" placeholder="Name" id="name-product-<?php echo strtolower($lang->languages_name); ?>" required>
+                  <label for="short-product-<?php echo strtolower($lang->languages_name); ?>">Short description</label>
+                  <textarea id="short-product-<?php echo strtolower($lang->languages_name); ?>" class="WYSIWYG-editor form-control" required></textarea>
+                  <label for="long-product-<?php echo strtolower($lang->languages_name); ?>">Long description</label>
+                  <textarea id="long-product-<?php echo strtolower($lang->languages_name); ?>" class="WYSIWYG-editor form-control" required></textarea>
+                </fieldset>
+                <?php
+                }
+                ?>
+                
+              </form>
+            </div>
+            <div class="modal-footer">
+              <button type="button" class="btn btn-secondary" data-dismiss="modal">Cancel</button>
+              <button type="button" class="btn btn-primary" id="yesSaveProduct">Save</button>
+            </div>
+          </div>
+        </div>
+      </div>
+
       <script type="text/javascript" src="js/jquery-3.2.1.min.js"></script>
+
+      <!-- Bootstrap 4 external dependecy -->
       <script type="text/javascript" src="https://npmcdn.com/tether@1.2.4/dist/js/tether.min.js"></script>
       <script type="text/javascript" src="js/bootstrap4.min.js"></script>
+      <!-- <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/1000hz-bootstrap-validator/0.11.9/validator.js"></script> -->
+
+      <!-- Froala external js dependencies -->
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/codemirror.min.js"></script>
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/codemirror/5.25.0/mode/xml/xml.min.js"></script>
+   
+      <!-- Froala js -->
+      <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/froala-editor/2.6.0/js/froala_editor.pkgd.min.js"></script>
+
       <script type="text/javascript" src="js/custom.js"></script>
     </body>
   </html>
